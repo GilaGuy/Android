74e9dc544f41e0ee6b250d8f052413c71c9073db
Melvin Loho <mloho@outlook.com>
2015-03-13 16:04:52 -0700

    Revert "Client's send should NOW be working..."
    This reverts commit 3c6bcbcfe08d4a806568a076ba8ba93dd0d55fdf.

--- Source/Client/Project/app/src/main/java/com/logic_team_chris/android_gps/MainActivity.java
+++ Source/Client/Project/app/src/main/java/com/logic_team_chris/android_gps/MainActivity.java
@@ -70,22 +70,9 @@ public class MainActivity extends ActionBarActivity {
             dgram = new DatagramPacket(PacketData, PacketData.length, Addr, ServerPort);
 
             // Send the packet
-            new AsyncTask<Void, String, Void>() {
-                @Override
-                protected Void doInBackground(Void... params) {
-                    try {
-                        ClientSocket.send(dgram);
-                    } catch (Exception e) {
-                        publishProgress(e.getMessage());
-                    }
-                    return null;
-                }
-
-                @Override
-                protected void onProgressUpdate(String... progress) {
-                    Toast.makeText(MainActivity.this, progress[0], Toast.LENGTH_LONG).show();
-                }
-            };
+            try {
+                ClientSocket.send(dgram);
+            } catch (Exception e) {}
         }
 
         @Override
